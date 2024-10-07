01ee4db4cc477667621c54fb0c351ee1884984a4
Ilona <venusflytrapd.d@gmail.com>
2024-10-07 14:08:19 +0300

    Recognition of different instruments In Progress
    Faced ray detector problem

--- Assets/Scripts/Instruments&Cursor/CursorBehaviour.cs
+++ Assets/Scripts/Instruments&Cursor/CursorBehaviour.cs
@@ -5,13 +5,59 @@ using UnityEngine;
 public class CursorBehaviour : MonoBehaviour
 {
     public Instrument instrument;
+
+    [SerializeField] private LayerMask _interactableLayer;
+
+
+    private bool _holdingInstrument = false;
+
     private void Awake()
     {
-        TakeInstrument(instrument);
+
     }
+
+    private void Update()
+    {
+        if (_holdingInstrument)
+            return;
+
+        if(Input.GetMouseButtonDown(0)) //If mouse left clicked
+        {
+            Debug.Log(" left clicked");
+            TryToDetectInstrument();
+        }
+    }
+
+    //Cursor private methods
+    /// <summary>
+    /// Shooting raycast and trying to get an instrument
+    /// </summary>
+    private void TryToDetectInstrument()
+    {
+        var mousePos = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        ray.direction *= 100f; // making ray longer
+        if (Physics.Raycast(ray, out RaycastHit hitInfo))
+        {
+            try
+            {
+                Debug.Log("GOT!");
+                hitInfo.transform.GetComponent<Instrument>().Take();
+            }
+            catch
+            {
+                Debug.LogWarning("Can't get Instrument component from " + hitInfo.transform.name);
+            }
+        }
+    }
+
+
+
     private void TakeInstrument(Instrument instrument)
     {
         Cursor.visible = false;
         instrument.Take();
+        Debug.Log("Took instrument");
     }
+
 }
