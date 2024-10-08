a5bfc7c101bd3bb45c7c5cde397fce769da39e10
Milla-J <milla.jerohin@vuoksi.fi>
2024-10-08 11:53:46 +0300

    Wall hit detection

--- Assets/Scripts/Instruments&Cursor/Brush.cs
+++ Assets/Scripts/Instruments&Cursor/Brush.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class Brush : Instrument
 {
+    public GameManager gameManager;
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Rust")
+        {
+            gameManager.AddToSatisfaction();
+            gameManager.fixing = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Rust")
+        {
+            gameManager.fixing = false;
+        }
+    }
 }
