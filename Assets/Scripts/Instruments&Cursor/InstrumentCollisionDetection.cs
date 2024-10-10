1d63010f6db6b51be86b1ac82ca65633ff3f1539
Ilona <venusflytrapd.d@gmail.com>
2024-10-10 14:16:43 +0300

    Triggers on instruments are not colliding

--- Assets/Scripts/Instruments&Cursor/InstrumentCollisionDetection.cs
+++ Assets/Scripts/Instruments&Cursor/InstrumentCollisionDetection.cs
@@ -10,10 +10,9 @@ public class InstrumentCollisionDetection : Instrument
 
     private List<Collider2D> collidersInTrigger = new List<Collider2D>();
 
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        //check if instrument touches wall
-        if (other.tag == "Wall")
+        if (collision.gameObject.tag == "Wall")
         {
             Debug.Log("Instrument touched wall");
             if (collidersInTrigger.Count == 0)
@@ -21,21 +20,21 @@ public class InstrumentCollisionDetection : Instrument
                 gameManager.StartCoroutine(gameManager.LowerSatisfaction());
             }
 
-            if (!collidersInTrigger.Contains(other))
+            if (!collidersInTrigger.Contains(collision.collider))
             {
-                collidersInTrigger.Add(other);
+                collidersInTrigger.Add(collision.collider);
             }
         }
     }
 
-    void OnTriggerExit2D(Collider2D other)
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        if (other.tag == "Wall")
+        if (collision.gameObject.tag == "Wall")
         {
             Debug.Log("Instrument touched wall Exit");
-            if (collidersInTrigger.Contains(other))
+            if (collidersInTrigger.Contains(collision.collider))
             {
-                collidersInTrigger.Remove(other);
+                collidersInTrigger.Remove(collision.collider);
             }
 
             if (collidersInTrigger.Count <= 0)
@@ -45,4 +44,39 @@ public class InstrumentCollisionDetection : Instrument
             }
         }
     }
+    //void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    //check if instrument touches wall
+    //    if (other.tag == "Wall")
+    //    {
+    //        Debug.Log("Instrument touched wall");
+    //        if (collidersInTrigger.Count == 0)
+    //        {
+    //            gameManager.StartCoroutine(gameManager.LowerSatisfaction());
+    //        }
+
+    //        if (!collidersInTrigger.Contains(other))
+    //        {
+    //            collidersInTrigger.Add(other);
+    //        }
+    //    }
+    //}
+
+    //void OnTriggerExit2D(Collider2D other)
+    //{
+    //    if (other.tag == "Wall")
+    //    {
+    //        Debug.Log("Instrument touched wall Exit");
+    //        if (collidersInTrigger.Contains(other))
+    //        {
+    //            collidersInTrigger.Remove(other);
+    //        }
+
+    //        if (collidersInTrigger.Count <= 0)
+    //        {
+    //            //Debug.Log("No longer touching wall");
+    //            gameManager.StopLowerSatisfaction();
+    //        }
+    //    }
+    //}
 }
